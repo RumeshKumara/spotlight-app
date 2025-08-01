dc128f54b84382e3fe9b3dce4d5cbb3bec86ff3d
rumesh <rumeshk066@gmail.com>
2025-08-01 14:32:03 +0530

    created tab bar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,45 +1,56 @@
 import { Tabs } from 'expo-router';
 import React from 'react';
-import { Platform } from 'react-native';
+import { View, Text } from 'react-native';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
+export default function TabsLayout() {
   return (
     <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
-      <Tabs.Screen
-        name="index"
+        screenOptions={{
+            tabBarShowLabel: false,
+            headerShown: false,
+            tabBarActiveTintColor: '#249900',
+            tabBarInactiveTintColor: '#999999',
+            tabBarStyle: {
+                backgroundColor: '#161616',
+                borderTopWidth: 0,
+                elevation: 0,
+                height: 40,
+                position: 'absolute',
+                paddingBottom: 8,
+            },
+           
+        }}
+    >
+      <Tabs.Screen name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          tabBarIcon: () => <Ionicons name="home" size={24} color="#249900" />,
+        }}
+      
+      />
+       <Tabs.Screen name="bookmarks"
+        options={{
+          tabBarIcon: () => <Ionicons name="bookmark" size={24} color="#249900" />,
         }}
       />
-      <Tabs.Screen
-        name="explore"
+      <Tabs.Screen name="create" 
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          tabBarIcon: () => <Ionicons name="add-circle" size={24} color="#249900" />,
+        }}
+      
+      />
+      <Tabs.Screen name="notifications"
+        options={{
+          tabBarIcon: () => <Ionicons name="notifications" size={24} color="#249900" />,
         }}
       />
+     
+      <Tabs.Screen name="profile"
+        options={{
+          tabBarIcon: () => <Ionicons name="person" size={24} color="#249900" />,
+        }}
+      />
+      
     </Tabs>
   );
 }
